5e6d54442ea39f2ca65fc93e43b2ff88285e8110
btfrost <btfrost20@gmail.com>
2019-11-26 17:09:54 -0600

    Added test cases for restaurant name, address, URL, phone number, and rating

--- SeeFood/src/SeeFoodTest.java
+++ SeeFood/src/SeeFoodTest.java
@@ -42,5 +42,94 @@ class SeeFoodTest {
 		assertEquals(server.getRestaurant(73, 32186.9).get(0), "Javier's Gourmet Mexicano");
 		assertEquals(server.getRestaurant(73, 32186.9).get(1), "4912 Cole Avenue 75205");
 	}
+	
+	@Test
+	void testRestaurantName() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(1, 1609.34);
+        	assertNotNull(restaurantInfo.get("name"));
+    	}
 
+    	@Test
+    	void testRestaurantName2() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(25, 1609.34);
+		assertNotNull(restaurantInfo.get("name"));
+    	}
+
+	@Test
+	void testRestaurantName3() {
+		HashMap<String, String> restaurantInfo = server.getRestaurant(148, 1609.34);	
+		assertNotNull(restaurantInfo.get("name"));
+	}
+
+    	@Test
+    	void testRestaurantAddress() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(60, 1609.34);
+        	assertNotNull(restaurantInfo.get("address"));
+    	}
+
+    	@Test
+    	void testRestaurantAddress2() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(70, 1609.34);
+        	assertNotNull(restaurantInfo.get("address"));
+    	}
+
+    	@Test
+    	void testRestaurantAddress3() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(73, 1609.34);
+        	assertNotNull(restaurantInfo.get("address"));
+    	}
+
+    	@Test
+    	void testRestaurantUrl() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(1, 8046.72);
+        	assertNotNull(restaurantInfo.get("url"));
+    	}
+	
+	@Test
+    	void testRestaurantUrl2() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(25, 8046.72);
+        	assertNotNull(restaurantInfo.get("url"));
+    	}
+
+    	@Test
+    	void testRestaurantUrl3() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(148, 8046.72);
+        	assertNotNull(restaurantInfo.get("url"));
+    	}
+
+    	@Test
+    	void testRestaurantPhoneNumbers() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(60, 8046.72);
+        	assertNotNull(restaurantInfo.get("phoneNumbers"));
+    	}
+
+    	@Test
+    	void testRestaurantPhoneNumbers2() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(70, 8046.72);
+        	assertNotNull(restaurantInfo.get("phoneNumbers"));
+    	}
+	
+	@Test
+    	void testRestaurantPhoneNumbers3() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(73, 8046.72);
+        	assertNotNull(restaurantInfo.get("phoneNumbers"));
+    	}
+
+    	@Test
+    	void testRestaurantRating() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(1, 16093.4);
+        	assertNotNull(restaurantInfo.get("rating"));
+    	}
+
+    	@Test
+    	void testRestaurantRating2() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(25, 16093.4);
+        	assertNotNull(restaurantInfo.get("rating"));
+    	}
+
+    	@Test
+    	void testRestaurantRating3() {
+        	HashMap<String, String> restaurantInfo = server.getRestaurant(48, 16093.4);
+        	assertNotNull(restaurantInfo.get("rating"));
+    	}
 }
